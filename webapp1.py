bc9da1443c7b6ced164f6b593b8f06fa7538093d
Monir EL OUARROUDI <95141609+ElouarrM@users.noreply.github.com>
2024-09-10 21:42:32 +0100

    Update webapp1.py

--- webapp1.py
+++ webapp1.py
@@ -9,7 +9,7 @@ weather_data = []
 # Function to update the weather data
 def update_weather_data(new_data):
     global weather_data
-    weather_data=new_data
+    weather_data = new_data
 
 @app.route('/receiveData', methods=['POST'])
 def receive_data():
@@ -22,7 +22,7 @@ def receive_data():
 def get_weather_data():
     return jsonify(weather_data)
 
-# Serve the HTML page
+# Serve the HTML page with the button
 @app.route('/')
 def display_weather_page():
     # HTML template embedded in the Flask route using render_template_string
@@ -32,11 +32,32 @@ def display_weather_page():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Weather Data</title>
+        <title>Weather Forecast</title>
         <style>
             table {width: 100%; border-collapse: collapse;}
             th, td {border: 1px solid black; padding: 8px; text-align: center;}
             th {background-color: #f2f2f2;}
+            .button {
+                background-color: #4CAF50; /* Green */
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 20px auto;
+                cursor: pointer;
+                width: 100%;
+                max-width: 93%;
+                border-radius: 10px;
+            }
+             h1, h2 {
+            text-align: center;
+        }
+            .button:hover {
+                background-color: #45a049;
+            }
         </style>
         <script>
             function fetchWeatherData() {
@@ -67,7 +88,16 @@ def display_weather_page():
         </script>
     </head>
     <body>
-        <h1>Weather Data</h1>
+         <h1>Weather Forecast</h1>
+    <a href="https://login.salesforce.com/" class="button">Search Weather Data</a>
+
+    <div class="weather-info">
+        <h2>Current Weather</h2>
+        <p><strong>Temperature:</strong> 17.6°C</p>
+        <p><strong>Wind Speed:</strong> 3.8 m/s</p>
+    </div>
+
+    <h2>Hourly Forecast</h2>
         <table>
             <thead>
                 <tr>
