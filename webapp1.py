dc886d343fde5c1720f01c8d11759e3b0635c74c
Monir EL OUARROUDI <95141609+ElouarrM@users.noreply.github.com>
2024-09-10 19:11:22 +0100

    Update webapp1.py

--- webapp1.py
+++ webapp1.py
@@ -1,26 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-def print_data(a):
-    print(a)
+# Variable globale pour stocker les données
+weather_data = {}
+
 @app.route('/receiveData', methods=['POST'])
 def receive_data():
+    global weather_data
     data = request.get_json()
-    print_data(data)
-    print('monir')
-    # Process the data here if needed
+    weather_data = data  # Stocker les données dans la variable globale
     return jsonify({"status": "success", "data": data})
-
-# Assurez-vous que cette route est correcte dans votre application Flask
-@app.route('/slave.html')
-def weather_forecast():
-    # Votre logique ici
-    pass
 
 @app.route('/master.html')
 def master():
-    return render_template('master.html')
+    global weather_data
+    return render_template('master.html', data=weather_data)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=10000)
+    app.run(debug=False, host='0.0.0.0', port=10000)
