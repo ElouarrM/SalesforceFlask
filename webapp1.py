00b19a1a37914ced726383add071346be222a914
Monir EL OUARROUDI <95141609+ElouarrM@users.noreply.github.com>
2024-09-10 19:24:15 +0100

    Update webapp1.py

--- webapp1.py
+++ webapp1.py
@@ -1,21 +1,31 @@
 from flask import Flask, request, jsonify, render_template
+import json
+import os
 
 app = Flask(__name__)
 
-# Variable globale pour stocker les données
-weather_data = {}
+DATA_FILE = 'weather_data.json'
+
+def read_data_from_file():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as file:
+            return json.load(file)
+    return {}
+
+def write_data_to_file(data):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
 
 @app.route('/receiveData', methods=['POST'])
 def receive_data():
-    global weather_data
     data = request.get_json()
-    weather_data = data  # Stocker les données dans la variable globale
+    write_data_to_file(data)  # Stocker les données dans le fichier JSON
     return jsonify({"status": "success", "data": data})
 
 @app.route('/master.html')
 def master():
-    global weather_data
-    return render_template('master.html', data=weather_data)
+    data = read_data_from_file()  # Lire les données du fichier JSON
+    return render_template('master.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=10000)
